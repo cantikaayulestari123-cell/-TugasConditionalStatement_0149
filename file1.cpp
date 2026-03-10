879fc91a5cd28a115504db77ea92aa13aad281c4
cantikaayulestari123-cell <cantikaayulestari123@gmail.com>
2026-03-10 22:31:40 +0700

    menambahkan fungsi untuk menghitung nilai

--- file1.cpp
+++ file1.cpp
@@ -1,3 +1,8 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+//Fungsi untuk menghitung nilai BMI
+double hitungBMI(double berat,double tinggi) {
+    return berat / (tinggi * tinggi);
+}
